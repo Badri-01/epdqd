7f913672542d14334288f75694b836e5aa1e7eb1
Badrinadh <47272974+Badri-01@users.noreply.github.com>
2020-11-26 17:00:15 +0530

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -14,11 +14,14 @@ public class Simulation {
     public static void main(String args[]) {
         Thread ccm = new Thread(new CCM());
         ccm.start();
-        Thread rsu = new Thread(new RSU());
+        Thread rsu = new Thread(new RSU(9000,9001));
         //rsu.start();
-        Runnable Va = new Vehicle(1);
+        Runnable Va = new Vehicle(1,9000,9001);
         Thread V1 = new Thread(Va);
+        Runnable Vb = new Vehicle(2,9000,9001);
+        Thread V2 = new Thread(Vb);
         V1.start();
+        V2.start();
         
     }
 
